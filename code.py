e84f8d500fceb1d37d52eb23fe253cc0903a1965
ahmadtarig <ahmedtarig.email@gmail.com>
2024-04-24 21:35:29 +0200

    remove a stop words

--- code.py
+++ code.py
@@ -1,3 +1,26 @@
-file = open('paragraphs.txt', 'r')
-data = file.read()
-file.close()
+file = open('paragraphs.txt', 'r')  # Read a file
+data = file.read()  # Save a file content in variable "data"
+
+data = data.split()  # Convert data to list of words
+
+print(data[:50])
+# Define a list of stop words
+stopWords = [
+    "an", "and", "are", "as", "at", "be", "by", "for", "from",
+    "has", "he", "in", "is", "it", "its", "of", "on", "that", "the",
+    "to", "was", "were", "will", "with"
+]
+
+# Remove a stop words from data list but this code does not work
+# for stopWord in stopWords:
+#     while stopWord in data:
+#         data.remove(stopWord)
+# so I am using this Chat-gpt code
+
+# Remove stop words from data using list comprehension
+data = [word for word in data if word.lower() not in stopWords]
+
+# Join data in string
+data = " ".join(data)
+
+file.close()  # close a file
