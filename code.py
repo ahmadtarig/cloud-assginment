1a0fdfd246738d47a816404055673c85fe36fcf4
ahmadtarig <ahmedtarig.email@gmail.com>
2024-04-24 22:20:41 +0200

    calc frequency of each word

--- code.py
+++ code.py
@@ -3,7 +3,6 @@ data = file.read()  # Save a file content in variable "data"
 
 data = data.split()  # Convert data to list of words
 
-print(data[:50])
 # Define a list of stop words
 stopWords = [
     "an", "and", "are", "as", "at", "be", "by", "for", "from",
@@ -20,7 +19,28 @@ stopWords = [
 # Remove stop words from data using list comprehension
 data = [word for word in data if word.lower() not in stopWords]
 
-# Join data in string
-data = " ".join(data)
+# join data in text
+text = " ".join(data)
+
+print(text)
+
+
+# Create frequency dict
+frequency = {}
+
+# Same error I don't know why does not work logically code is true
+# for word in data:
+#     print(word)
+#     if word in frequency["words"]:
+#         frequency["frequency"][frequency["words"].index(word)] += 1
+#     else:
+#         frequency["words"].append(word)
+#         frequency["frequency"].append(1)
+
+# So I am using chat-gpt code
+
+# calc frequency
+for word in data:
+    frequency[word] = frequency.get(word, 0) + 1
 
 file.close()  # close a file
